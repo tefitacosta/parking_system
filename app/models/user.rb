6aa71e7af2b18aa90c22775da0416687aacf7260
tefitacosta <tefitacosta@gmail.com>
2016-07-26 19:58:44 -0500

    Formulario vehiculo nuevo listo

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,19 +4,18 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
     has_many :vehicles
-    
+
   validates :first_name, presence: true, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }
   validates :last_name, presence: true, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }
   validates :email, presence: true, uniqueness: true, format:  /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, length: { maximum: 254 }
   validates :password, presence: true, confirmation: true
   validates :password_confirmation, presence: true
   validates :birthdate, presence: true, :timeliness => {:on_or_before => lambda { Date.current }, :type => :date}
-#    validates :username, format: { with: /regex/ }
-  before_save :is_admin
-    
+# validates :username, format: { with: /regex/ }
+  after_save :is_admin
+
     private
     def is_admin
        is_admin = false
     end
-end  
-  
+end
